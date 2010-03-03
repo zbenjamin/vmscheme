8f63eb0610c348861f4c18f2a45a115bdbfc9781
Zev Benjamin <zev@mit.edu>
2010-03-03 00:16:55 -0600

    file parsing function

--- src/parser.c
+++ src/parser.c
@@ -1,7 +1,11 @@
 #include <ctype.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include <object.h>
 #include <primitive_procedures.h>
@@ -128,4 +132,37 @@ parse_symbol(char **p, const char *end)
   memcpy(sym, start, len);
   sym[len] = '\0';
   return make_symbol(sym);
+}
+
+struct object*
+parse_file(const char *filename)
+{
+  int fd = open(filename, O_RDONLY);
+  if (fd < 0) {
+    int error = errno;
+    printf("Could not load '%s': %s\n", filename, strerror(error));
+    exit(1);
+  }
+
+  ssize_t bufsize = 1024;
+  char *buf = malloc(sizeof(char) * bufsize);
+  char *pos = buf;
+  ssize_t offset = pos - buf;
+  size_t nbytes = 1023;
+  ssize_t bytes_read = 0;
+  while ((bytes_read = read(fd, pos, nbytes)) != 0) {
+    pos += bytes_read;
+    offset = pos - buf;
+    if (offset + nbytes >= bufsize) {
+      char *new_buf = malloc(sizeof(char) * bufsize * 2);
+      memcpy(new_buf, buf, bufsize);
+      bufsize *= 2;
+      free(buf);
+      buf = new_buf;
+      pos = new_buf + offset;
+    }
+  }
+  *pos = '\0';
+
+  return parse(buf);
 }
--- src/parser.h
+++ src/parser.h
@@ -2,5 +2,5 @@
 #define VMSCHEME_PARSER_H
 
 struct object* parse(char *str);
-
+struct object* parse_file(const char *filename);
 #endif // VMSCHEME_PARSER_H
